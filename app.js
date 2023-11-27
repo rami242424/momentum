f81c104f88c1ca479907156622302d14da28cb3e
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-27 23:46:38 +0900

    2.2 Variable

---
+++ app.js
@@ -0,0 +1,15 @@
+// const  : 이 친구는 variable을 만들 것이니 준비해야겠군
+// 1. 변수 없이 작성시
+console.log(5 + 2); // 7
+console.log(5 * 2); // 10
+console.log(5 / 2); // 2.5
+
+// 2. 변수로 작성시
+const a = 5;
+const b = 2;
+const myName = "rami";
+
+console.log(a + b); // 7
+console.log(a * b); // 10
+console.log(a / b); // 2.5
+console.log("hello " + myName); // hello rami
