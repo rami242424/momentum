fa6a15034fe2fb13deecda33601d4082cc56b93c
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-28 00:41:50 +0900

    2.3 const and let

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 // const  : 이 친구는 variable을 만들 것이니 준비해야겠군
 // 1. 변수 없이 작성시
-console.log(5 + 2); // 7
-console.log(5 * 2); // 10
-console.log(5 / 2); // 2.5
+// console.log(5 + 2); // 7
+// console.log(5 * 2); // 10
+// console.log(5 / 2); // 2.5
 
-// 2. 변수로 작성시
+// // 2. 변수로 작성시
+// const a = 5;
+// const b = 2;
+// const myName = "rami";
+
+// console.log(a + b); // 7
+// console.log(a * b); // 10
+// console.log(a / b); // 2.5
+// console.log("hello " + myName); // hello rami
+
+// const 와 let 의 차이
+// const : 상수(constant) / 값이 바뀔 수 없다.
+// let : when i want to create new one / 새롭게 생성할때 사용
+
 const a = 5;
 const b = 2;
-const myName = "rami";
+let myName = "rami";
 
 console.log(a + b); // 7
 console.log(a * b); // 10
 console.log(a / b); // 2.5
-console.log("hello " + myName); // hello rami
+console.log("hello " + myName);
+
+// myName을 바꾸로 싶다면,
+myName = "rami Oh";
+console.log(myName);
