8d807a9d3fc89cd12fa32988994ac5358d77c2dc
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-28 20:00:51 +0900

    2.4 Boolean

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-// const  : 이 친구는 variable을 만들 것이니 준비해야겠군
-// 1. 변수 없이 작성시
-// console.log(5 + 2); // 7
-// console.log(5 * 2); // 10
-// console.log(5 / 2); // 2.5
+// string
+// const amIFat = "true";
 
-// // 2. 변수로 작성시
-// const a = 5;
-// const b = 2;
-// const myName = "rami";
+// boolean
+const amIFat = true;
+console.log(amIFat);
 
-// console.log(a + b); // 7
-// console.log(a * b); // 10
-// console.log(a / b); // 2.5
-// console.log("hello " + myName); // hello rami
+// null 그 변수에 아무것도 없다는 뜻(값이 없다)
 
-// const 와 let 의 차이
-// const : 상수(constant) / 값이 바뀔 수 없다.
-// let : when i want to create new one / 새롭게 생성할때 사용
-
-const a = 5;
-const b = 2;
-let myName = "rami";
-
-console.log(a + b); // 7
-console.log(a * b); // 10
-console.log(a / b); // 2.5
-console.log("hello " + myName);
-
-// myName을 바꾸로 싶다면,
-myName = "rami Oh";
-console.log(myName);
+//undefined : 컴퓨터 메모리 안에는 존재, 공간은 있으나 값이 들어가지 않은 것
+let something;
+console.log(something);
