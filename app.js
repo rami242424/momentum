f5a1ad40ba2642a0ae3481649ba741866caadd82
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-28 20:26:16 +0900

    2.5 Arrays

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-// string
-// const amIFat = "true";
-
-// boolean
-const amIFat = true;
-console.log(amIFat);
-
-// null 그 변수에 아무것도 없다는 뜻(값이 없다)
-
-//undefined : 컴퓨터 메모리 안에는 존재, 공간은 있으나 값이 들어가지 않은 것
-let something;
-console.log(something);
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[2]); //
