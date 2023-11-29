d9de356a137c2a2ee32dec714bcc8d790d77aea3
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-29 11:28:30 +0900

    function의 사용

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-// console.log("Hello my name is Rami");
-// console.log("Hello my name is Nico");
-// console.log("Hello my name is Bori");
-// console.log("Hello my name is Lynn");
-// console.log("Hello my name is Luca");
+console.log("Hello my name is Rami");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Bori");
+console.log("Hello my name is Lynn");
+console.log("Hello my name is Luca");
 
-// 반복된 코드가 너무 많다. -> function 필요
-// 코드를 캡슐화해서 실행을 여러번 할 수 있게 해준다.
-// function 원하는이름(){
-//     실행하게 될 코드 블록
-// }
+반복된 코드가 너무 많다. -> function 필요
+코드를 캡슐화해서 실행을 여러번 할 수 있게 해준다.
+function 원하는이름(){
+    실행하게 될 코드 블록
+}
 
 function sayHello(){
     console.log("hello");
 }
+// function 실행하기
+sayHello();
+sayHello();
+sayHello();
+// hello
+// hello
+// hello
