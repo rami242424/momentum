984d00994ca082a8d9d16d813d6bcc929a3fdc68
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-29 11:28:07 +0900

    function의 사용

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const toBuy = ["potato", "tomato", "pizza"];
-console.log(toBuy[2]); // pizza
-console.log(toBuy[68768787]) // undefined => 아무것도 없다(정의되지않음)
+// console.log("Hello my name is Rami");
+// console.log("Hello my name is Nico");
+// console.log("Hello my name is Bori");
+// console.log("Hello my name is Lynn");
+// console.log("Hello my name is Luca");
+
+// 반복된 코드가 너무 많다. -> function 필요
+// 코드를 캡슐화해서 실행을 여러번 할 수 있게 해준다.
+// function 원하는이름(){
+//     실행하게 될 코드 블록
+// }
+
+function sayHello(){
+    console.log("hello");
+}
