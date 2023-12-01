d3340a5ce94ed71d3e28e83f4e201581f8c438ec
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-01 21:26:40 +0900

    toggle

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked!");
-    h1.style.color = "blue";
+    // const clickedClass = "clicked"; 
+    // toggle로 클래스를 한번만 사용하기 때문에 정의할 필요가 없다.
+    h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter(){
-    h1.innerText = "mouse is here";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "mouse is gone";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-
-// 여기서 h1은 HTML element이다
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-// 여기서부터는 window!
-
-window.addEventListener("resize", handleWindowResize);
-
-window.addEventListener("copy", handleWindowCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
