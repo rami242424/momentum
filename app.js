870dc328e575740fe0410eec7334c7052f8dc567
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-01 23:09:06 +0900

    231201 코드챌린지 완성본

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 function updateSize() {
     const containerWidth = window.innerWidth;
-    
-    if (containerWidth === 552) {
-        document.body.style.backgroundColor = "blue";
+    const bodyClassList = document.body.classList;
+
+    // 기존에 설정된 모든 배경색 클래스 제거
+    bodyClassList.remove('blue-background', 'red-background', 'green-background');
+
+    if (containerWidth <= 400) {
+        bodyClassList.add('blue-background');
+    } else if (containerWidth <= 600) {
+        bodyClassList.add('red-background');
     } else {
-        document.body.style.backgroundColor = "red";
+        bodyClassList.add('green-background');
     }
 }
 
