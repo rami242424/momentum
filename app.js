0e2fbf84d238db94f1c29e986e7193fc3a855c31
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-03 11:02:30 +0900

    const와 console.log

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-function plus() {
-    console.log(2 + 2);
+const player ={
+    name: "rami",
+    age: 24,
 }
+// console.log(player.name, player.age);
 
-plus();
-plus();
+const calculator = {
+    add: function(a, b){
+        console.log(a, b);
+    },
+};
+
+calculator.add(5, 1); // 5 1
+// console.log(calculator.add); // ƒ (a, b){console.log(a, b);}
+// console.log(calculator.add()); // undefined undefined > undefined
+// calculator.add(); // undefined undefined
