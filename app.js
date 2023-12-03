794a9cb4140ff054e4bc96e39fcf48b8357e3f2f
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-03 11:21:29 +0900

    calculator_nico ver.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const calculator = {
-    add: function(a, b){
+    plus: function(a, b){
         console.log(a + b);
     },
     minus: function(a, b){
@@ -8,16 +8,16 @@ const calculator = {
     divide: function(a, b){
         console.log(a / b);
     },
-    multiply: function(a, b){
+    times: function(a, b){
         console.log(a * b);
     },
-    powerof: function(a, b){
+    power: function(a, b){
         console.log(a ** b);
     },
 };
 
-calculator.add(5, 1); 
+calculator.plus(5, 1); 
 calculator.minus(5, 1); 
 calculator.divide(15, 5); 
-calculator.multiply(5, 3); 
-calculator.powerof(5, 2); 
+calculator.times(5, 3); 
+calculator.power(5, 2); 
