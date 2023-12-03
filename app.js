92a04637886b2b06ff62a8d71376e6e82d64b1d9
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-03 10:44:14 +0900

    function

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-function updateSize() {
-    const containerWidth = window.innerWidth;
-    const bodyClassList = document.body.classList;
-
-    // 기존에 설정된 모든 배경색 클래스 제거
-    bodyClassList.remove('blue-background', 'red-background', 'green-background');
-
-    if (containerWidth <= 400) {
-        bodyClassList.add('blue-background');
-    } else if (containerWidth <= 600) {
-        bodyClassList.add('red-background');
-    } else {
-        bodyClassList.add('green-background');
-    }
+function plus() {
+    console.log(2 + 2);
 }
 
-window.addEventListener("resize", updateSize);
-updateSize(); // 페이지 로드 시에도 함수를 호출하여 초기 배경색 설정
+plus();
+plus();
