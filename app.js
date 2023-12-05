d4286f94386eb52d9a08a977b2a7f1a3cb9cfe8d
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-05 14:26:11 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting"); 
+
 const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event){
     event.preventDefault();
-    // 1.유저가 이름을 제출 한 뒤에 form없애기
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    // 2. h1추가하기
-    // greeting.innerText = "Hello " + username; // 아래와 같음
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME); // "hidden" > HIDDEN_CLASSNAME 수정시 "" 지우는거 잊지말기!!
+    localStorage.setItem(USERNAME_KEY, username);
+    // greeting.innerText = `Hello ${username}`;
+    // greeting.classList.remove(HIDDEN_CLASSNAME); 
+    paintGreetings(username);
 
-
+} 
+// 2. 중복되는 문장 함수로
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`; // savedUsername은 argument에서 가져올것
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// loginForm.addEventListener("submit", onLoginSubmit); // 이동 전
+    
+// 1. localStorage에 username이 있나 보기
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+// console.log(savedUsername); // 아무것도 없으면 null이 출력된다.
+if (savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME); // show the username
+    loginForm.addEventListener("submit", onLoginSubmit); // 이동 후
+} else {
+    // greeting.classList.remove(HIDDEN_CLASSNAME);// show the greetings
+    // // greeting.classList.add(innerText); // 이게 아니라 아래식.. 왜 이렇게 생각했을까?
+    // greeting.innerText = `Hello ${savedUsername}`;
+    paintGreetings(savedUsername);
+}
 
+// localStorage에 유저정보가 있으면 savedUsername을 받아서 인자로 넣어주고
+// 유저정보가 없다면, form의 submit을 기다리고 -> loginForm.addEventListener("submit", onLoginSubmit); -> form이 submit되면 -> 인풋으로 부터 유저정보를 받고 const username = loginInput.value; -> 인풋에서 받은 user를 가진 paintingGreeting을 호출한다. paintGreetings(username);
+ 
