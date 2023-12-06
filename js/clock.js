2873e87d1fd6b35796711e5395b191c5b7533ce1
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-06 17:55:17 +0900

    5.0 setInterval

---
+++ js/clock.js
@@ -0,0 +1,4 @@
+const clock = document.querySelector("h2#clock");
+// const clock = document.querySelector("#clock");
+// const clock = document.getElementById("clock");
+// clock.innerText = "hahah"
