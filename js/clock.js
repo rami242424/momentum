93f1e05709fd266caca368354876aa4a7eb3eba4
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-06 20:58:06 +0900

    크리스마스이브 디데이

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,33 @@
-const clock = document.querySelector("h2#clock");
-// const clock = document.querySelector("#clock");
-// const clock = document.getElementById("clock");
-// clock.innerText = "hahah"
+const clockTitle = document.querySelector(".js-clock");
+
+function getClock() {
+  // 오늘 지금 이시간
+  const date = new Date();
+
+  // 크리스마스 이브
+  const christmasEve = new Date(date.getFullYear(), 11, 24, 0, 0, 0, 0);
+
+  // 남은 시간 = christmasEve - date
+  const timeLeft = christmasEve - date;
+
+  const seconds = String(Math.floor(timeLeft / 1000) % 60).padStart(2, "0");
+  const minutes = String(Math.floor(timeLeft / (1000 * 60)) % 60).padStart(
+    2,
+    "0",
+  );
+  const hours = String(Math.floor(timeLeft / (1000 * 60 * 60)) % 24).padStart(
+    2,
+    "0",
+  );
+  const days = String(Math.floor(timeLeft / (1000 * 60 * 60 * 24))).padStart(
+    2,
+    "0",
+  );
+
+  // 구해야 되는 값 (christmasEve - date)
+  const remainingTimeStr = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  clockTitle.innerText = remainingTimeStr;
+}
+
+getClock();
+setInterval(getClock, 1000);
