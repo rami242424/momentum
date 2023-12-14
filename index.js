7cceadbb5eede784c3e796e46fe934a023930b39
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-14 22:57:22 +0900

    functions part

---
+++ index.js
@@ -0,0 +1,13 @@
+// function sayHello(nameOfPerson) {
+//     console.log(nameOfPerson);
+// }
+
+// sayHello("nico");
+// sayHello("dal");
+// sayHello("lynn");
+
+function addNumbers( ) {
+    console.log(a + b);
+}
+
+addNumbers(1); 
