944400d65a5b192760e9485238671af7e4863b52
Rami <138556019+rami242424@users.noreply.github.com>
2023-12-17 00:28:10 +0900

    231217

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// function sayHello(nameOfPerson) {
-//     console.log(nameOfPerson);
-// }
-
-// sayHello("nico");
-// sayHello("dal");
-// sayHello("lynn");
-
-function addNumbers( ) {
-    console.log(a + b);
+function plus(){
+    console.log(2 + 2); // 1. 이렇게 function안에 직접 값을 넣는것 보다
 }
 
-addNumbers(1); 
+// plus // nothing happend
+// plus(); // 4
+plus(2, 5)
