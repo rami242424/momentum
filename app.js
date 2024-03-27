310bce482b85f38629222f7358b208a56b571bbd
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-27 21:30:58 +0900

    const와 let

---
+++ app.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 2;
+let myName = "rami";
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + "my name is " + myName);
+
+myName = "nico";
+console.log("my new name is " + myName);
--- index.js
+++
@@ -1,7 +0,0 @@
-function plus(){
-    console.log(2 + 2); // 1. 이렇게 function안에 직접 값을 넣는것 보다
-}
-
-// plus // nothing happend
-// plus(); // 4
-plus(2, 5)
