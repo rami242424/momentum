720759efe4dfd88bf31a01a56891dca0604e7265
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-27 21:40:11 +0900

    Boolean

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-const a = 5;
-const b = 2;
-let myName = "rami";
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + "my name is " + myName);
+const amIFat = false;
+console.log(amIFat);
 
-myName = "nico";
-console.log("my new name is " + myName);
+// null : 아무것도 없음
+// undefined : 메모리에 있지만, 값이 없다(정의x)
