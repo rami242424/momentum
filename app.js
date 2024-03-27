a2bd5f5741f94024b308c593297b011fe2a9b499
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-27 22:23:31 +0900

    Array 와 다른 Object

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const amIFat = false;
-console.log(amIFat);
+// Array와 다른 Object (프로퍼티를 가진 데이터를 저장해준다.)
+// 방법1
+// const playerName = "rami";
+// const playerPoint = 100;
+// const playerFat = false;
 
-// null : 아무것도 없음
-// undefined : 메모리에 있지만, 값이 없다(정의x)
+// 방법2
+const player = {
+    name : "rami",
+    point : 100,
+    fat : false,
+};
+
+console.log(player); // {name: 'rami', point: 100, fat: false}
+// console.log(player.name); // rami
+// console.log(player["name"]); // rami
+
+player.fat = true; // fat 이 false > true로 수정됨
+player.firstName = "oh"; // firstName 이 추가됨
+player.point = player.point + 10; // 기존값 100에 10이 추가됨
+console.log(player); // {name: 'rami', point: 110, fat: true, firstName: 'oh'}
+
+
