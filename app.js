07d57846125368056a59cdb254dc517b48cc32de
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 20:54:12 +0900

    4.3 events part two

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const link = document.querySelector("a");
 const loginForm = document.querySelector("#login-form");
 const loginInput =document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
@@ -9,5 +10,13 @@ function onLoginSubmit(event){
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+    // console.log(event);
+    // alert("clicked"); // 기본동작을 포함한 모든 동작들이 막혀있다가, alert에서 ok를 누르면 다시 기본동작이 실행된다.
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
  
