d78a8674d3c8237a0ba77a2597c1d6faa4fd2089
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 17:17:20 +0900

    event

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-// const h = document.getElementsByTagName("h1");
-// console.log(h); // HTMLCollection [h1] (배열값)-> 원하는 값이 아님...
-// querySelector 는 element를 CSS 방식으로 검색한다!!
+const h1 = document.querySelector("div.hello:first-child h1");
 
-// const h = document.querySelector("h1");
-// 또는 
-// const h = document.querySelector(".hello h1");
-// h1이 여러개 일때 사용하면 첫번째 h1값만 나온다.
-// const h = document.querySelector(".hello h1:first-child");
 
-//id로 찾을때(html에 id 정의해줘야함, 지금 class만 정의)
-// const h = document.querySelector("#hello");
-// const h2 = document.getElementById("hello");
+function handleTitleClick(){
+    // console.log("title was clicked");
+    h1.style.color = "red";
+}
 
-// console.log(h);
-// console.log(h2);
+function handleMouseEnter(){
+    h1.innerText ="Mouse is here";
+}
 
-const title = document.querySelector(".hello h1");
+function handleMouseLeave(){
+    h1.innerText ="Mouse is gone";
+}
 
-console.log(title);
-title.innerText = "helllo llll"; // 브라우저에 Grap me!-1 으로 출력되었던 것이 helllo llll로 바뀜
+function handleWindowResize(){
+    document.body.style.background = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!!");
+}
+
+h1.addEventListener("click", handleTitleClick);
+// title.onclick = handleTitleClick
+h1.addEventListener("mouseenter", handleMouseEnter);
+// title.onmouseenter = handleMouseEnter
+h1.addEventListener("mouseleave", handleMouseLeave);
+// title.onmouseleave = handleMouseLeave
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
