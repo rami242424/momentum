3a7d321ea6ca91679be714ee40e4bd954ac78b19
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 15:25:23 +0900

    conditionals part three, true, false if else, else if

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const age = parseInt(prompt("how old are you?"));
-// console.log(isNaN(age)); // 숫자가 아니면 true, 숫자면 false => 
-// 15 입력, false 출력 => 입력된게 숫자다
-// aaa 입력, true 출력 => 입력된게 숫자가 아니다
 
-// if (condition) { // condition 은 boolean 이어야한다 즉, true 또는 false 중 1
-//     // condition === true 일때 실행할 코드
-// } else {
-//     // condition === false 일때 실행할 코드
-// }
-// ---
-
-if (isNaN(age)) {
-    // isNaN(age) === true 일때 실행 될 코드, 즉 입력된게 숫자가 아닐때 실행됨
-    alert("plz write a number");    
-} else {
-    // isNaN(age) === false 일때 실행 될 코드, 즉 입력된게 숫자일때 실행됨
-    alert("your age is " + age);
+if (isNaN(age) || age < 0 ) {
+    alert("plz write a real positive number");    
+} else if(age < 18) {
+    alert("you are too young to drink");
+} else if(age >= 18 && age <= 50){
+    alert("you can drink");
+} else if (age > 50 && age <= 80){
+    alert("Do not drink for your healthy");
+} else if (age > 80){
+    alert("you can do whatever you want");
 }
+
