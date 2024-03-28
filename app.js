de7ef7033abf0bc7a01fa519206f58db8c52b983
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 18:07:15 +0900

    classList 와 contains, remove, add 로 토글기능

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // css를 사용하여 글자색을 바꿔주고, 자바스크립트를 이용하여 className을 토글식으로 만들수있다.
+// className은 이전 class들을 상관하지 않고, 모든걸 교체해버린다 => 버그 발생, classList 사용으로 버그 수정 (h1.className === clickedClass)=> (h1.classList.contains(clickedClass)로 수정
+
 
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    // h1.className = "active";
-    // console.log(h1.className);
-    const clickedClass = "clicked sexy-font";
-    if (h1.className === clickedClass){
-        h1.className = "";
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
