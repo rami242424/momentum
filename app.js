0920e2098c4037880fe2124205075c93ede338f5
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 21:40:05 +0900

    4.7 super recap

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const loginInput =document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault(); 
@@ -14,19 +15,37 @@ function onLoginSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
 
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
 
-    greeting.innerText = "hello " + username;
+    // greeting.innerText = "hello " + username;
     // 아래와 같은 코드
     // greeting.innerText = `hello  ${username}`;
 
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
     // console.log(username);
+    paintGreetings(username);
+}
+
+// 중복 없애기
+function paintGreetings(username, ){
+    greeting.innerText = "hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+// console.log(savedUsername);
 
- 
+if (savedUsername === null) { // localstorage에 유저정보가 없을때,
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    // greeting.innerText = "hello " + savedUsername;
+    // greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(savedUsername);
+}
