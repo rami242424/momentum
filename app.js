bfb9bf8b1bd5ba4cb96f6e1887086d3bdf512b45
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 17:34:22 +0900

    3.6 css in jvs 띵작

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 
+// function handleTitleClick(){
+//     // console.log(h1.style.color);
+//     // h1.style.color = "blue";
+//     // console.log(h1.style.color);
+//     if (h1.style.color === "blue") {
+//         h1.style.color = "tomato";
+//     } else {
+//         h1.style.color = "blue";
+//     }
+// }
+
+// 위에 식의 중복 없애기
 function handleTitleClick(){
-    // console.log("title was clicked");
-    h1.style.color = "red";
-}
+    const currentColor = h1.style.color;
+    let newColor;
 
-function handleMouseEnter(){
-    h1.innerText ="Mouse is here";
-}
-
-function handleMouseLeave(){
-    h1.innerText ="Mouse is gone";
-}
-
-function handleWindowResize(){
-    document.body.style.background = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!!");
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-// title.onclick = handleTitleClick
-h1.addEventListener("mouseenter", handleMouseEnter);
-// title.onmouseenter = handleMouseEnter
-h1.addEventListener("mouseleave", handleMouseLeave);
-// title.onmouseleave = handleMouseLeave
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
+// h1.style.color = newColor => The first one changes the color of h1
+// newColor = h1.style.color => The second one saves the color of the h1 in a variable.
