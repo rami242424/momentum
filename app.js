d767fe51b5edf2a2d51006bb0817ed063ebb727a
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 17:57:50 +0900

    3.7

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
+// css를 사용하여 글자색을 바꿔주고, 자바스크립트를 이용하여 className을 토글식으로 만들수있다.
+
 const h1 = document.querySelector("div.hello:first-child h1");
 
-
-// function handleTitleClick(){
-//     // console.log(h1.style.color);
-//     // h1.style.color = "blue";
-//     // console.log(h1.style.color);
-//     if (h1.style.color === "blue") {
-//         h1.style.color = "tomato";
-//     } else {
-//         h1.style.color = "blue";
-//     }
-// }
-
-// 위에 식의 중복 없애기
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    // h1.className = "active";
+    // console.log(h1.className);
+    const clickedClass = "clicked sexy-font";
+    if (h1.className === clickedClass){
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
-
-// h1.style.color = newColor => The first one changes the color of h1
-// newColor = h1.style.color => The second one saves the color of the h1 in a variable.
