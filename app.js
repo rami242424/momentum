47e3fdf52c5225d7336e5cc1b2919115a71fccc5
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 19:44:26 +0900

    4.0 input value

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// css를 사용하여 글자색을 바꿔주고, 자바스크립트를 이용하여 className을 토글식으로 만들수있다.
-// className은 이전 class들을 상관하지 않고, 모든걸 교체해버린다 => 버그 발생, classList 사용으로 버그 수정 (h1.className === clickedClass)=> (h1.classList.contains(clickedClass)로 수정
+// const loginForm = document.getElementById("login-form");
+// const loginInput =loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+// 위 코드와 같음
+const loginInput =document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+function onLoginBtnClick(){
+    // console.log(loginInput.value);
+    // console.log("click");
+    if (loginInput.value !== ""){
+       console.log("hello " + loginInput.value);
+    } 
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
