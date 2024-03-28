cc60f08f80bb05355246d2c1fad7e1dcb494c106
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 21:11:19 +0900

    4.4 getting username

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+// 4.4 유저가 이름 제출한 뒤, 입력값 hidden하기
+
+const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 const loginForm = document.querySelector("#login-form");
 const loginInput =document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
-    // const username = loginInput.value;
-    event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막아주는 것
-    console.log(loginInput.value);
+    event.preventDefault(); 
+    // console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = "hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // console.log(username);
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-    // console.log(event);
-    // alert("clicked"); // 기본동작을 포함한 모든 동작들이 막혀있다가, alert에서 ok를 누르면 다시 기본동작이 실행된다.
-}
+
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
  
