9c6eb55fb62f2bf8b15e985908bc6c15b46862b0
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 20:27:53 +0900

    4.2 events, preventDefault로 브라우저 기본동작 막기

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput =loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
-// 위 코드와 같음
+const loginForm = document.querySelector("#login-form");
 const loginInput =document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username);
-
-    // if (username === ""){
-    //     alert("please write your name");
-    // } else if (username.length > 15){
-    //     alert("your name is too long");
-    // } 
-    // 대신 html input에서 설정가능
+function onLoginSubmit(event){
+    // const username = loginInput.value;
+    event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막아주는 것
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+ 
