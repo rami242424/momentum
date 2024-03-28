f3fdbbe504c65a29a7d7628e37cb1fa59b99f735
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-28 16:07:55 +0900

    HTML in javascript

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const age = parseInt(prompt("how old are you?"));
+// const title = document.getElementById("title");
+const title = document.getElementsByClassName("title");
 
-if (isNaN(age) || age < 0 ) {
-    alert("plz write a real positive number");    
-} else if(age < 18) {
-    alert("you are too young to drink");
-} else if(age >= 18 && age <= 50){
-    alert("you can drink");
-} else if (age > 50 && age <= 80){
-    alert("Do not drink for your healthy");
-} else if (age > 80){
-    alert("you can do whatever you want");
-}
-
+// console.dir("innerHTML =>" + title.innerHTML);
+// console.dir("innerText =>" + title.innerText);
+// console.dir("textContent =>" + title.textContent);
+// console.dir(title);
+title.innerText = "Got you";
