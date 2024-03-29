7b22a13dcba497054517791491e1cf4fc49eca46
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-29 21:29:12 +0900

    7.3 saving to dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){ // toDos []의 내용을 로컬스토리지에 넣기 위해 만든 함수
+    // localStorage.setItem("todos", toDos);
+    localStorage.setItem("todos", JSON.stringify(toDos)); // string으로 바꿔줌
+}
+
 function deleteToDo(event){
     // console.log(event.target.parentElement);
     const li = event.target.parentElement; // 삭제하고 싶은 li
@@ -26,8 +33,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo); // 이걸 로컬스토리지에 담고싶다 but 배열은 불가하고 텍스트만 저장할 수 있다.
     paintToDo(newTodo);
-    
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
