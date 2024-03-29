0e100c186bffdaf764f0127850d1f9cea2783467
Rami <138556019+rami242424@users.noreply.github.com>
2024-03-29 20:48:36 +0900

    7.2 deleteing to do

--- js/todo.js
+++ js/todo.js
@@ -1,23 +1,30 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-// const toDoInput = document.querySelector("#todo-form input"); // 위코드와 일치
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    // console.log(event.target.parentElement);
+    const li = event.target.parentElement; // 삭제하고 싶은 li
+    li.remove();
+}
+
 function paintToDo(newTodo){ // todo를 그리는 역할 담당
-    // console.log("i will paint", newTodo);
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
-    // console.log(li);
+
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
-function handleToDoSubmit(event){ // 자바스크립트가 방금 발생한 event를 handleToDoSubmit의 첫번째 인자로 준다
+function handleToDoSubmit(event){ 
     event.preventDefault();
-    // console.log(toDoInput.value);
-    const newTodo = toDoInput.value; // 입력한 인풋값 저장하기
-    // toDoInput.value 를 입력하고 난뒤에 인풋칸에 적힌 글자들 지우기
+    const newTodo = toDoInput.value;
     toDoInput.value = "";
     paintToDo(newTodo);
     
