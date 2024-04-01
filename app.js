778459f521a0eda6a9d2280cbe3d34332ef1cc3e
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 17:33:07 +0900

    2.11 return

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
+// const age = 96;
+// function calculateKrAge(ageOfForeigner){
+//     return ageOfForeigner + 2;
+// }
+
+// // console.log(?);
+// const krAge = calculateKrAge(age)
+// console.log(krAge); // 98
+
+// ---
+// const age = 96;
+// function calculateKrAge(ageOfForeigner){
+//     ageOfForeigner + 2;
+//     return "hello";
+// }
+
+// const krAge = calculateKrAge(age)
+// console.log(krAge); // hello
+
+// ---
 const calculator = {
-    plus : function(potato, carrot){
-        console.log("plus = " + (potato + carrot));
-    },
-
-    divide : function(potato, carrot){
-        console.log("divide = " + (potato / carrot));
-    },
-
-    times : function(potato, carrot){
-        console.log("times = " + (potato * carrot));
-    },
-
-    minus : function(potato, carrot){
-        console.log("minus = " + (potato - carrot));
+    plus : function (a, b) {
+        return a + b;
     },
 };
 
-calculator.plus(5, 5); // plus = 10
-calculator.divide(5, 5); // divide = 1
-calculator.times(5, 5); // times = 25
-calculator.minus(5, 5); // minus = 0
+const plusResult = calculator.plus(5, 5);
+// console.log(plusResult);
+
+
+
+
+
+
+
+
+
+
