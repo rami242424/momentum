b8a431c380862cf6a925dd1048eb99cc85aa49e4
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 22:16:09 +0900

    3.7 css in jas two

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    // console.log(h1.style.color); // getter
-    // h1.style.color = "blue"; // setter
-    // console.log(h1.style.color); // getter
-    // ---------------------
- 
-    const currentColor = h1.style.color;
-    let newColor;
+    const clickedClass = "clicked";
+   if (h1.className === clickedClass){
+    h1.className = "";
+   } else {
+    h1.className = clickedClass;
+   }
 
-    if(currentColor === "blue") { /// === 일치함을 확인하는 용도
-        newColor = "tomato"; // = 수정
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
