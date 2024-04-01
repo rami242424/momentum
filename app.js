a458f81d610d0fbef7de89bb9416e780dc1546bb
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 17:57:29 +0900

    2.14 Conditionals part two

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
-// const age = 96;
-// function calculateKrAge(ageOfForeigner){
-//     return ageOfForeigner + 2;
-// }
+// const age = prompt("how old r u")
+// console.log(parseInt(age));
+// string -> number : string 사용!!
+  
+const age = prompt("how old r u");
 
-// // console.log(?);
-// const krAge = calculateKrAge(age)
-// console.log(krAge); // 98
-
-// ---
-// const age = 96;
-// function calculateKrAge(ageOfForeigner){
-//     ageOfForeigner + 2;
-//     return "hello";
-// }
-
-// const krAge = calculateKrAge(age)
-// console.log(krAge); // hello
-
-// ---
-const calculator = {
-    plus : function (a, b) {
-        return a + b;
-    },
-};
-
-const plusResult = calculator.plus(5, 5);
-// console.log(plusResult);
-
-
-
-
-
-
-
-
-
-
+if (isNaN(age)){
+    console.log("plz write a number"); 
+}
