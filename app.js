dec7898ed375a9d6a04a4eea7ddc49c7c2e304bd
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 21:18:57 +0900

    3.1 html in jas

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const age = parseInt(prompt("how old r u"));
+const title = document.getElementById("title");
+console.dir(title);
 
-if (isNaN(age) || age <0){
-    console.log("plz write a number"); 
-} else if (age < 18){
-    console.log("you are too young");
-} else if (age >= 18 && age <= 50){
-    console.log("you can drink");
-} else if (age > 50 && age <= 80){
-    console.log("you should exercise");
-} else if (age === 100){
-    console.log("wow you are wise"); // 실행됨. age에 parseInt 안하면 동작X
-} else if (age > 80){
-    console.log("you can do whatever you want");
-}
-// } else if (age == 100) {
-//     console.log("wow you are wise"); // 실행안됨 why? 80이상이다 = 100
-// }
+title.innerText = "어쩌라고";
+console.log(title.id);
+console.log(title.innerText);
+console.log(title.autofocus);
+console.log(title.className);
