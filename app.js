4fa850f7fd955360b0279759f4928f143bce794e
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 12:15:19 +0900

    2.5 Arrays

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const mon = "mon1";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+// const daysOfWeek = mon + tue
+// console.log(daysOfWeek); // mon1tue
+
+// const daysOfWeek = [mon, tue]
+// console.log(daysOfWeek); //  ['mon1', 'tue']
+// console.log(daysOfWeek[0]); // mon1
+
+const daysOfWeek = ["mon", "tue", "wed"];
+console.log(daysOfWeek); // ['mon', 'tue', 'wed']
+daysOfWeek.push("thur");
+console.log(daysOfWeek);
