5aae69090a282d9b2a778bc0898e4c3ac1a0f97c
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 22:22:21 +0900

    3.8 css in jas three ClassList 사용하기

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
+// classList : class들의 목록으로 작업할 수 있게 허용해줌
+// className : 모든걸 교체해버림(기존것 삭제)
 function handleTitleClick(){
     const clickedClass = "clicked";
-   if (h1.className === clickedClass){
-    h1.className = "";
+   if (h1.classList.contains(clickedClass)){
+    h1.classList.remove(clickedClass);
    } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
    }
 
 }
