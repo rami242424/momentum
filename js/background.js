2b195676f242c4b3bd5242680403f68142fbc70c
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-01 11:52:42 +0900

    2.2 Variables '10' = string

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["1.jpg", "2.jpg", "22.jpg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-// console.log(chosenImage);
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-// console.log(bgImage);
-
-document.body.appendChild(bgImage);
