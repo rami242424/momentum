f085f2f0aeae2240b1847e274545d982dbda84cd
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-02 22:30:54 +0900

    5.2 PadStart

--- 240402.js
+++ 240402.js
@@ -1,7 +1,3 @@
-// padStart, padEnd는 string과 사용가능
-
-const clock = document.querySelector("h2#clock");
-
 function getClock(){
     const date = new Date();
     // clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
@@ -11,9 +7,3 @@ function getClock(){
     const sec = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${mins}:${sec}`;
 }
-
-// setInterval(sayHello, 1000); // 설정한 매 초마다 반복
-// setTimeout(sayHello, 1000); // 설정한 초 후에 한번 실행
-
-getClock(); // 이 코드가 없으면 1초 뒤부터 시간이 업데이트 된다.
-setInterval(getClock, 1000);
