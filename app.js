4639ef70b8e82cbee8ea7a10ba020f548d58831d
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-02 17:34:45 +0900

    4.2 Events -> 어렵다 다시꼭볼것

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
 
-function onLoginBtnClick(){
-    const username = loginInput.value;
-    console.log(username)   ;
+function onLoginSubmit(event){ // submit은 엔터를 누르거나 버튼을 클릭할 때 발생한다.
+    event.preventDefault(); // 브라우저의 기본 동작 막아주기
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+// 미션1 form을 submit할때 입력값을 받아내는것(submit event 감지)
+// 미션2 submit후 새로고침되는것 막기(윈도우 기본동작)
