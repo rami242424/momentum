ec9648db53e4c542728a02a36cb681ebe7332e35
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-03 10:38:16 +0900

    7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoInput = toDoForm.querySelector("input"); // 위 코드와 동일
 const toDoList = document.getElementById("todo-list");
-// console.log(toDoForm);
+
+function paintToDo(newTodo){
+    // console.log("i will paint " + newTodo);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    // console.log(li);
+    toDoList.appendChild(li);
+}
+
 
 function handleToDoSubmint(event){
     event.preventDefault();
-    // console.log(toDoInput.value);
-    const newTodo = toDoInput.value;
-    console.log("저장하고" + toDoInput.value);
-    toDoInput.value = "";
-    console.log("삭제하고" + newTodo, "비우고" + toDoInput.value);
+    const newTodo = toDoInput.value; // todo input값 저장
+    toDoInput.value = ""; // input 비우기
+    paintToDo(newTodo); // 저장한 input값을 넣어 paintToDo 함수 호출
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmint);
