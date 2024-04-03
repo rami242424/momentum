efd71ade4d560050b0e4b91a7c26c2a30691e619
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-03 16:50:02 +0900

    7.5 방법2

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,9 @@ const toDoForm = document.getElementById("todo-form");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoInput = toDoForm.querySelector("input"); // 위 코드와 동일
 const toDoList = document.getElementById("todo-list");
-const toDos = [];
+// const toDos = []; 새롭게 저장된 리스트들도 toDos에 저장하기위해 아래코드로변경
+let toDos = [];
+
 const TODOS_KEY = "todos";
 
 function saveToDos(){
@@ -42,10 +44,10 @@ function handleToDoSubmint(event){
 toDoForm.addEventListener("submit", handleToDoSubmint);
 
 
-
-function sayHello(item){
-    console.log("This is the turn of ", item);
-}
+// 아래 shortcut으로 작성도 가능
+// function sayHello(item){
+//     console.log("This is the turn of ", item);
+// }
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 // console.log(savedToDos); // array지만 string
@@ -54,6 +56,18 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     console.log(parsedToDos); // 이용가능한 array => 각각의 함수도 적용 가능
-    parsedToDos.forEach(sayHello); // forEach array의 각 item에 대해 함수를 실행
+    // parsedToDos.forEach(sayHello); // forEach array의 각 item에 대해 함수를 실행 (아래 코드로 shortcut도 가능)
+    // parsedToDos.forEach((item) => console.log("this is the turn of ", item)); (결국 아래코드처럼 paintToDo 함수를 실행하면 됨)
+
+    toDos = parsedToDos;
+
+    parsedToDos.forEach(paintToDo); // 결국  작성한 todo리스트들에 함수를 쓰고싶은거니까.
+
+
+    // -----------------------------여기까지의 함수로 로컬스토리지에 저장된 item값들이 화면에 출력된 상태를 유지함 but 새로운 todo list를 작성하면 기존에 작성되어 로컬스토리지에 저장되어있던 item들은 사라지고, 새로운 list들만 업데이트 된다. --> const toDos = []; 로 시작되기 때문!!
+
+
+
+
 
 }
