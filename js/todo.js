8bdaa35da0eaf4544773ce5e4170b4f16624b9cd
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-03 11:03:56 +0900

    7.2 Deleting To Dos : event.target.parentElement

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); // 위 코드와 동일
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    // console.log("지웠다"); // 어떤 버튼이 눌렸는지 모른다.
+    // console.log(event.composedPath()); // path 볼수 있는 코드
+    // console.log(event.target.parentElement);
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
-    // console.log("i will paint " + newTodo);
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
-    // console.log(li);
+    const btn = document.createElement("button");
+    btn.innerText = "X";
+    btn.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
     toDoList.appendChild(li);
 }
 
